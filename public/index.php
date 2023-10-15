fdf58976cf6bf2453cec6c282d3da24a5056f6b4
黃文輝 <antory040512@gmail.com>
2023-10-15 14:23:28 +0800

    增加首頁跟css檔案

--- public/index.php
+++ public/index.php
@@ -1,55 +1,62 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta viewport" content="width=device-width, initial-scale=1.0">
+<title>首頁</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>    
+<body>
+<header class="page-header">
+    <nav>
+        <ul class="nav-list">
+            <li class="nav-item dropdown">
+                <a href="mailto:antory040512@gmail.com" style="color: black;">聯絡我們</a>
+                <ul class="dropdown-content">
+                    <?php
+                        session_start();
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+                        if (isset($_SESSION["login_session"])) 
+                        {
+                            if ($_SESSION["login_session"] != true) 
+                            {
+                                echo "<li class='nav-item'><a href='login.php' style='color:black'>登入</a></li>";
+                            } 
+                            else if ($_SESSION["manager_login_session"] == true )
+                            {
+                                echo "<li class='nav-item'><a href='manager.php' style='color: black'>回主頁</a></li>";
+                            }
+                            else
+                            {
+                                echo "<li class='nav-item'><a href='user.php' style='color: black'>回主頁</a></li>";
+                            }
+                        } 
+                        else 
+                        {
+                            echo "<li class='nav-item'><a href='login.php' style='color: black'>登入</a></li>";
+                        }
+                    ?>
+                </ul>
+            </li>
+            <li class="nav-item"><a href='history.php' style="color: black;">歷史公告</a></li>
+        </ul>
+    </nav>
+</header>
 
-define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+   <section id="location">
+    <div class="wrapper">
+        <dic class ="location-info">
+            <h3 class "sub-title">天筑精舍</h3>
+            <p>
+                地址:335桃園市大溪區內柵路一段98巷54-1號<br>
+                電話:033883257<br>
+            </p>
+        </div>
+        <div class="location-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3620.1942296287816!2d121.27671427444545!3d24.85721504538914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3468176912c5eaa7%3A0x46b0f53d5ae5cb0!2z5aSp562R57K-6IiN!5e0!3m2!1szh-TW!2stw!4v1696680829001!5m2!1szh-TW!2stw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+    </section>
+</body>
+<html>
